5fa2cbce9a4bd3ca7d15aa9149b1ea78ce334072
Sultik03 <sultanaubakirov8@gmail.com>
2026-04-07 23:15:50 +0500

    finally finishing MyLinkedList class

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+//this is a doubly linked list btw
 
 public class MyLinkedList<T> implements MyList<T> {
     private class MyNode { //node class
@@ -82,78 +83,160 @@ public class MyLinkedList<T> implements MyList<T> {
 
 
     @Override
-    public void addFirst(T item) {
-        
-    }
-
-    @Override
-    public void addLast(T item) { //add element at beginning
+    public void addFirst(T item) { //add element at beginning
         MyNode newNode = new MyNode(item);
         if (head == null) {
             head = tail = newNode;
         } else {
-
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
         }
+        size++;
+    }
 
+    @Override
+    public void addLast(T item) { //add element at enf
+        MyNode newNode = new MyNode(item);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        return head.data; //returns the first element
     }
 
     @Override
     public T getLast() {
-        return null;
+        return tail.data; //returns the last element
     }
 
     @Override
-    public void removeFirst() {
-
+    public void removeFirst() { //remove first element
+        if (head == null) {
+            return;
+        }
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        size--;
     }
 
     @Override
     public void removeLast() {
-
+        if (tail == null) {
+            return;
+        }
+        tail = tail.prev;
+        if (tail != null) {
+            tail.next = null;
+        } else {
+            head = null;
+        }
+        size--;
     }
 
     @Override
-    public void sort() {
-
+    public void sort() { //bubble sort
+        if (size < 2) {
+            return;
+        }
+        for (int i = 0; i < size - 1; i++) {
+            MyNode current = head;
+            for (int j = 0; j < size - i - 1; j++) {
+                Comparable a = (Comparable) current.data;
+                Comparable b = (Comparable) current.next.data;
+                if (a.compareTo(b) > 0) {
+                    T temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                }
+                current = current.next;
+            }
+        }
     }
 
     @Override
-    public int indexOf(Object object) {
-        return 0;
+    public int indexOf(Object object) { //first occurence
+        MyNode current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.data.equals(object)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object object) {
-        return 0;
+        MyNode current = tail;
+        int index = size - 1;
+        while (current != null) {
+            if (current.data.equals(object)) {
+                return index;
+            }
+            current = current.prev;
+            index--;
+        }
+        return -1;
     }
 
     @Override
     public boolean exists(Object object) {
-        return false;
+        return indexOf(object) != -1;
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
+    public Object[] toArray() { //converts to array
+        Object[] arr = new Object[size];
+        MyNode current = head;
+        int i = 0;
+        while (current != null) {
+            arr[i++] = current.data;
+            current = current.next;
+        }
+        return arr;
     }
 
     @Override
-    public void clear() {
-
+    public void clear() { //clear list
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
-    public int size() {
+    public int size() { //returns the size
         return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<T>() {
+            private MyNode current = head;
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
     }
 }
