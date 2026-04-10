9186135a065a22db89f3d1f5a09f571b45a99403
Sultik03 <sultanaubakirov8@gmail.com>
2026-04-10 15:27:58 +0500

    finishing MyMinHeap

---
+++ src/MyMeanHeap.java
@@ -0,0 +1,49 @@
+public class MyMeanHeap<T extends Comparable<T>> {
+    private MyArrayList<T> heap = new MyArrayList<>();
+    public void add (T value) {
+        heap.add(value);
+        heapifyUp(heap.size() - 1);
+    }
+    public T remove() {
+        T root = heap.get(0);
+        heap.add(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        heapifyDown(0);
+        return root;
+    }
+    private void swap (int i, int j) {
+        T temp = heap.get(i);
+        heap.add(i, heap.get(j));
+        heap.add(j, temp);
+    }
+    private void heapifyUp (int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap.get(index).compareTo(hep.get(parent)) >= 0) {
+                break;
+            }
+            swap(index, parent);
+            index = parent;
+        }
+    }
+    public void heapifyDown(int index) {
+        int size = heap.size();
+        wihle (true) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+            if (left < size && heap.get(left).compareTo(heap.get(smallest)) < 0) {
+                smallest = left;
+            }
+            if (right < size && heap.get(right).compareTo(heap.get(smallest)) < 0) {
+                smallest = right;
+            }
+
+            if (smallest == index) {
+                break;
+            }
+            swap(index, smallest);
+            index = smallest;
+        }
+    }
+}
